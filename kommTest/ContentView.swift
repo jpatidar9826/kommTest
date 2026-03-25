4ca7db78adf03cb1545ad9699668b11ff1b20877
Jayant Patidar <jpatidar9826@gmail.com>
2026-03-25 12:28:07 +0530

    Initial Commit

---
+++ kommTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  kommTest
+//
+//  Created by Vikas Patidar on 25/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ kommTest/kommTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  kommTestApp.swift
+//  kommTest
+//
+//  Created by Vikas Patidar on 25/03/26.
+//
+
+import SwiftUI
+
+@main
+struct kommTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
